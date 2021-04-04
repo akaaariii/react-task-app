40db184f574e6f056d522db93dc7176b9020b607
akaaariii <caaari23@gmail.com>
2021-04-03 23:12:33 -0700

    added dnd func

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DragDropContext } from 'react-beautiful-dnd'
 import data from './data'
 import Column from './components/Column'
 
 const App = () => {
-  const onDragEnd = (result) => {
+  const [todos, setTodos] = useState(data)
 
+  const onDragEnd = (result) => {
+    // console.log(result);
+    const { destination, source, draggableId } = result;
+    if(!destination) {
+      return
+    }
+    if(destination.droppableId === source.droppableId && destination.index === source.index) {
+      return
+    }
+
+    const column = todos.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+
+    // Switch the order
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    }
+    const newTodos = {
+      ...todos,
+      columns: {
+        ...todos.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+    setTodos(newTodos);
   }
 
   return (
     <DragDropContext
       onDragEnd={onDragEnd}
     >
-      {data.columnOrder.map((columnId) => {
-        const column = data.columns[columnId];
-        const tasks = column.taskIds.map((taskId) => data.tasks[taskId]);
+      {todos.columnOrder.map((columnId) => {
+        const column = todos.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => todos.tasks[taskId]);
 
         return <Column key={column.id} column={column} tasks={tasks} />
       })}
