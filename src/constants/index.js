04ca457865a32c1d6e85162377e97633b3d16e16
akaaariii <caaari23@gmail.com>
2021-05-20 23:25:54 -0700

    Added constants

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+export const ADD_COLUMN = "ADD_COLUMN";
+export const ADD_TASK = "ADD_TASK";
+export const EDIT_TASK = "EDIT_TASK";
+export const DELETE_COLUMN = "DELETE_COLUMN";
+export const DELETE_TASK = "DELETE_TASK";
